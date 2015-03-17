841f9d5cc30b1f27ae486d83f92b805e6b51ed8d
astillwell <astillwell@stcharlessd.org>
2015-03-17 04:37:04 -0500

    new file:   Combat.py 	new file:   Skills.py 	new file:   __pycache__/monsterClass.cpython-32.pyc 	new file:   __pycache__/playerClass.cpython-32.pyc 	new file:   monsterClass.py 	new file:   playerClass.py

---
+++ Skills.py
@@ -0,0 +1,52 @@
+#Skills
+#Dennis Gordick
+#3/10/2015
+
+skills = {
+    'Acrobatics-Dex': 0,
+    'Appraise-Int': 0,
+    'Bluff-Cha': 0,
+    'Climb-Str': 0,
+    'Craft-Int': 0,
+    'Diplomacy-Cha': 0,
+    'Disable Device-Dex': 0,
+    'Disguise-Cha': 0,
+    'Escape Artist-Dex': 0,
+    'Fly-Dex': 0,
+    'Handle Animal-Cha': 0,
+    'Heal-Wis': 0,
+    'Intimidate-Cha': 0,
+    'Knowledge (Arcana)-Int': 0,
+    'Knowledge (Dungeoneering)-Int': 0,
+    'Knowledge (Engineering)-Int': 0,
+    'Knowledge (Geography)-Int': 0,
+    'Knowledge (History)-Int': 0,
+    'Knowledge (Local)-Int': 0,
+    'Knowledge (Nature)-Int': 0,
+    'Knowledge (Nobility)-Int': 0,
+    'Knowledge (Planes)-Int': 0,
+    'Knowledge (Religion)-Int': 0,
+    'Linguistics-Int': 0,
+    'Perception-Wis': 0,
+    'Perform-Cha': 0,
+    'Profession-Wis': 0,
+    'Ride-Dex': 0,
+    'Sense Motive-Wis': 0,
+    'Sleight of Hand-Dex': 0,
+    'Spellcraft-Int': 0,
+    'Stealth-Dex': 0,
+    'Survival-Wis': 0,
+    'Swim-Str': 0,
+    'Use Magic Device-Cha': 0,}
+
+abilities = {
+    'Strength': 0,
+    'Dexterity': 0,
+    'Constitution': 0,
+    'Intelligence': 0,
+    'Wisdom': 0,
+    'Charisma': 0}
+
+for i in (skills):
+    print (i)
+print (abilities)
